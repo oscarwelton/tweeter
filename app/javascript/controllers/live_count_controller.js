737d42ee8d877e1ac507f756f8005d3237ce3714
oscar welton <oscarwelton@gmail.com>
2023-05-08 14:00:36 +0100

    added live counter

---
+++ app/javascript/controllers/live_count_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="live-count"
+export default class extends Controller {
+  connect() {
+    const counter = document.getElementById("live-count");
+    const length = document.getElementById("tweet_generated_tweet").value.length;
+    counter.innerText = length;
+
+    if (length === 280) {
+      counter.style.color = "red";
+    }
+
+    addEventListener("keyup", (event) => {
+      console.log(event);
+      const length = document.getElementById("tweet_generated_tweet").value.length;
+      counter.innerText = length;
+
+      if (length === 280) {
+        counter.style.color = "red";
+      } else {
+        counter.style.color = "";
+      }
+    });
+
+    addEventListener("DOMContentLoaded", (event) => {
+      const length = document.getElementById("tweet_generated_tweet").value.length;
+      counter.innerText = length;
+    });
+  }
+}
