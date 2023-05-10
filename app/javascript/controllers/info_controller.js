4470af606bd9ae2e7e662ede9906b58ddec34fb8
oscar welton <oscarwelton@gmail.com>
2023-05-10 17:35:29 +0100

    added working popup window

--- app/javascript/controllers/info_controller.js
+++ app/javascript/controllers/info_controller.js
@@ -9,16 +9,19 @@ console.log("Hello from info_controller.js");
 const infoButton = document.querySelector("#info-button");
 const infoContainer = document.querySelector("#info-container");
 const content = document.querySelector(".content");
+const buttonContainer = document.querySelector("#button-container");
+const closeButton = document.getElementById("close-button");
 
 infoButton.addEventListener("click", () => {
-  if (infoContainer.style.display === "flex") {
-    infoContainer.style.display = "none";
-    content.style.opacity = "1";
-  } else {
     infoContainer.style.display = "flex";
+    infoButton.style.display = "none";
     content.style.opacity = "0.5";
     infoContainer.style.opacity = "1";
-    infoButton.style.opacity = "1";
-  }
+});
 
+closeButton.addEventListener("click", () => {
+  infoButton.style.display = "flex";
+  infoContainer.style.display = "none";
+  content.style.opacity = "1";
+  buttonContainer.style.opacity = "1";
 });
