bcb999cca9a6f661be8a84a0905bca25b9c1d671
oscar welton <oscarwelton@gmail.com>
2023-05-18 11:17:27 +0100

    added csp to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_content_security_policy
+
+  private
+
+  def set_content_security_policy
+    response.headers["Content-Security-Policy"] = "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'; frame-ancestors 'self' *;"
+  end
 end
